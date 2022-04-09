c018396af67d8b005138bc41ff04ed1a6c5f7001
LiseRochat <liserochat@live.fr>
2022-04-09 18:49:27 +0200

    Upgrade Link on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,8 @@
-    <footer>
+<footer>
         <ul>
-            <li><a href="index.html">Accueil</a></li>
-            <li><a href="projects.html">Projets</a>
-            <li><a href="about.html">A Propos</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="projects.php">Projets</a>
+            <li><a href="about.php">A Propos</a></li>
         </ul>
         <p>Rochat Lise 2022 - Tous droits réservés</p>
     </footer>
