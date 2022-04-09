92ca77a85833d1ddeb61924a508df06019072396
LiseRochat <liserochat@live.fr>
2022-04-09 14:57:57 +0200

    Add og:image:alt

--- templates/head.php
+++ templates/head.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ROCHAT Lise | Développeuse web et web mobile | Alternance 2022-2023 Développement</title>
-    <meta property="og:title" content="ROCHAT Lise | Développeuse web et web mobile" />
+    <meta property="og:title" content="ROCHAT Lise | Développeuse web | Recherche Alternance 2022-2023" />
     <meta property="og:url" content="https://liserochat.com" />
     <meta property="og:image" content="https://liserochat.com/assets/img/homepage.png" />
+    <meta property="og:image:alt" content="Page Accueil liserochat.com" />
     <meta name="description" content="Qui suis-je ? Développeuse web en formation, je recherche une alternance dans le domaine du développement. Voulant allier apprentissage et expérience, je développe des projets bénévolement.">
     <link rel="stylesheet" href="assets/style/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
